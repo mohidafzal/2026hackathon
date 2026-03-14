ab0d811feae0ddc47edf6a070ecda8d54a52e223
mohidafzal <mohidafzal31@gmail.com>
2026-03-14 12:43:35 -0500

    all methods except last one implemented

--- revenue_optimization/src/placementEngine.ts
+++ revenue_optimization/src/placementEngine.ts
@@ -31,7 +31,7 @@ export class PlacementEngine {
     }
 
     isAdCompatibleWithArea(ad: Ad, area: Area): boolean {
-        for(let i = 0 ; i <= ad.bannedLocations.length ; i++) {
+        for(let i = 0 ; i < ad.bannedLocations.length ; i++) {
             if(ad.bannedLocations[i] === area.location) {
                 return false;
             }
@@ -54,7 +54,9 @@ export class PlacementEngine {
         area: Area,
         startTime: number
     ): boolean {
-
+        if (startTime < 0 ) {
+            return false;
+        }
         if( (ad.timeReceived <= startTime) && 
             (startTime <= ad.timeReceived + ad.timeout) && 
             (area.timeWindow > startTime + ad.duration)) {
@@ -65,6 +67,16 @@ export class PlacementEngine {
     }
 
     isAdAlreadyScheduled(adId: string, schedule: Schedule): boolean {
+        for ( const key in schedule ) {
+            const areaSchedule = schedule[key];
+
+            for ( let i = 0 ; i < areaSchedule.length ; i++ ) {
+                if (adId === areaSchedule[i].adId) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -74,8 +86,40 @@ export class PlacementEngine {
         schedule: Schedule,
         startTime: number
     ): boolean {
-        return false;
+         
+        return this.isAdCompatibleWithArea(ad, area) && 
+        !this.isAdAlreadyScheduled(ad.adId,schedule) &&
+        this.doesPlacementFitTimingConstraints(ad,area,startTime) &&
+        this.privateNoOverlapHelper(ad,area,schedule,startTime);
     }
+
+    privateNoOverlapHelper(
+        ad: Ad,
+        area: Area,
+        schedule: Schedule,
+        startTime: number
+    ): boolean {
+        const areaSchedule = schedule[area.areaId] || [];
+        const newEndTime = ad.duration + startTime;
+
+        for (let i = 0 ; i < areaSchedule.length ; i++) {
+
+            const existingAd = areaSchedule[i];
+
+            const overlaps = 
+            startTime < existingAd.startTime &&
+            newEndTime > existingAd.startTime;
+
+            if(overlaps) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+        
+
+
 
     isAreaScheduleValid(area: Area, areaSchedule: ScheduledAd[], ads: Ad[]): boolean {
         return false;
